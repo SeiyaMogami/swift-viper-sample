f55769512de8ba1ec56c8a0b5533cba7f5b4cd18
最上 聖也 <mogami_s@mti.co.jp>
2017-09-06 20:35:20 +0900

    プロジェクト作成

---
+++ SwiftViperSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftViperSample
+//
+//  Created by 最上 聖也 on 2017/09/06.
+//  Copyright © 2017年 Seiya Mogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
