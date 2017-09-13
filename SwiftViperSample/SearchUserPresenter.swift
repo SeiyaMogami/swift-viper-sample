78bd3f10b1f5aab86b206fabcdb3b84ed066d9d5
最上 聖也 <mogami_s@mti.co.jp>
2017-09-13 14:08:22 +0900

    create files

---
+++ SwiftViperSample/SearchUserPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SearchUserPresenter.swift
+//  SwiftViperSample
+//
+//  Created by 最上 聖也 on 2017/09/13.
+//  Copyright © 2017年 Seiya Mogami. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftViperSample/SearchUserView.swift
@@ -0,0 +1,9 @@
+//
+//  SearchUserView.swift
+//  SwiftViperSample
+//
+//  Created by 最上 聖也 on 2017/09/13.
+//  Copyright © 2017年 Seiya Mogami. All rights reserved.
+//
+
+import Foundation
